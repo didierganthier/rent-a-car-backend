a9803dcdd4278c55ce2d80f150e57b7cb4116a0e
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-03-01 14:51:51 +0100

    update seeds.rb for initial rails db migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(name: 'John Jones', email: 'johnjones@gmail.com', password: '12345678', password_confirmation: '12345678')
+Car.create(name: 'Tesla Model S', car_type: 'rangerover', description: 'the fastest car in the market', image: 'https://tesla-cdn.thron.com/delivery/public/image/tesla/8a74d206-66dc-4386-8c7a-88ff32174e7d/bvlatuR/std/4096x2560/Model-S-Main-Hero-Desktop-LHD', brand: 'Tesla', daily_rate: '2324')
+Car.create(name: 'Tesla Model Y', car_type: 'landcruiser', description: 'the fastest car on land in the market', image: 'https://tesla-cdn.thron.com/delivery/public/image/tesla/91abd4c7-32a1-41cc-ade5-b64774dbea61/bvlatuR/std/2880x1800/Model-Y-Main-Hero-Desktop-Global?quality=auto-medium&amp;format=auto', brand: 'Tesla', daily_rate: '3000')
+Car.create(name: 'Tesla Model X', car_type: 'speedstar', description: 'the fastest car ever made', image: 'https://tesla-cdn.thron.com/delivery/public/image/tesla/8c26f779-11e5-4cfc-bd7c-dcd03b18ff88/bvlatuR/std/4096x2561/Model-X-Main-Hero-Desktop-LHD', brand: 'Tesla', daily_rate: '1234')
+Car.create(name: 'Tesla Model S', car_type: 'aportscar', description: 'the fastest sportscar in the market', image: 'https://tesla-cdn.thron.com/delivery/public/image/tesla/8a74d206-66dc-4386-8c7a-88ff32174e7d/bvlatuR/std/4096x2560/Model-S-Main-Hero-Desktop-LHD', brand: 'Tesla', daily_rate: '3224')
