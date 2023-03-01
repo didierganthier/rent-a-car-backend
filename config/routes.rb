76c9ec4217a59883248347b6edd25dac42d117c7
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-03-01 14:48:58 +0100

    Add routes for cars create index and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  # resources :users, only: [:create] 
+  resources :cars, only: [:index, :show, :create, :destroy]
   post '/login', to:  "users#login"
   post '/signup', to:  "users#signup"
 end
