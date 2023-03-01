78f1d4ee56cb531ddb599d4f4666c6a82f715436
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-03-01 20:33:38 +0100

    Add reservations routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :cars, only: [:index, :show, :create, :destroy]
+  resources :reservations, only: [:index, :create, :destroy]
   post '/login', to:  "users#login"
   post '/signup', to:  "users#signup"
 end
