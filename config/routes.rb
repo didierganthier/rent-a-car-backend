b429d9a01865e3558419385783cfff3b98072331
Shedrack-Sunday <hannania002@gmail.com>
2023-03-06 21:05:45 +0100

    Add integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :cars, only: [:index, :show, :create, :destroy]
   resources :reservations, only: [:index, :create, :destroy]
   post '/login', to:  "users#login"
